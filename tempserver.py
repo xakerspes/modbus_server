48b701b79709c6bbdf3eb872b5dc7ff584bd2f15
xakerspes <xakerspes@gmail.com>
2021-04-07 15:52:19 +0500

    sadasd

---
+++ tempserver.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+IP = ''
+PORT = 1010
+ADDR = (IP, PORT)
+SIZE = 1024
+DISCONNECT_MSG = "!DISCONNECT"
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+        conn.send("sdfdsfds")
+
+    conn.close()
+
+def main():
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+if __name__ == "__main__":
+    main()
