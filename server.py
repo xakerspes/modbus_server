c6f86be905baf10937ba0dd2fd068761afb6db1c
xakerspes <xakerspes@gmail.com>
2021-04-07 15:30:58 +0500

    asdasd

--- server.py
+++ server.py
@@ -1,14 +1,2 @@
 import socket
 import struct
-print("sdfdsf")
-print(":dsdfdsfdsf")
-print("sdfdsf")
-print(":dsdfdsfdsf")
-print("sdfdsf")
-print(":dsdfdsfdsf")
-print("sdfdsf")
-print(":dsdfdsfdsf")
-sdfdsfdsfdsf
-sdfdsfdsfdsf
-sdfdsfdsfdsfsdfdsf
-sdfdsfdsfdsf
